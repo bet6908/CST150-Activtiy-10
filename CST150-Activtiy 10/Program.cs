bfa796f0bf63168300405088fe095d95b69a3426
beto6 <beto6@DESKTOP-CBL37F1>
2022-04-13 22:56:14 -0600

    Add project files.

---
+++ CST150-Activtiy 10/Program.cs
@@ -0,0 +1,42 @@
+﻿//Corrected by <Roberto GonzalezRamirez>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST150_Activtiy_10
+{
+    class Program
+    {
+        static void Main( string[] args)
+        {
+            //Here we would need to attach a namespace to pull information from the Set class
+            //make some sets
+            //Set A = new Set();
+            //Set B = new Set();
+            CST150_Activtiy_10.Set A = new CST150_Activtiy_10.Set();
+            CST150_Activtiy_10.Set B = new CST150_Activtiy_10.Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //disbplay original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
---
+++ CST150-Activtiy 10/Set.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST150_Activtiy_10
+{
+    class Set
+    {
+        private List<int> elements;
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+
+
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                //if Val is not equal to elements then the statement
+                //would be false. No else statement required
+                if (val == elements[i])
+                    return true;
+                //else
+                // return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach(int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        //We would create set of union of A and B without altering the other sets
+        public Set union(Set rhs)
+        {
+            Set C = new Set();
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                C.addElement(rhs.elements[i]);
+                // this.addElement (rhs.elements[i]);
+            }
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                C.addElement(this.elements[i]);
+                // this.addElement (rhs.elements[i]);
+            }
+            return C;
+        }
+    }
+}
+
